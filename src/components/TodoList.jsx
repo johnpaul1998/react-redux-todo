076b23c432bec2f0382d7f62c6c1ac46f6618096
john paul araquel <103520276+johnpaul1998@users.noreply.github.com>
2022-08-27 11:24:36 +0800

    final commit
    final commit

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import TodoBody from "./TodoBody";
 import TodoHeader from "./TodoHeader";
+import * as actionLogin from "../redux/actions/actionLogin";
+import { useDispatch } from "react-redux/es/exports";
+import { bindActionCreators } from "redux";
+import { Button } from "react-bootstrap";
 
 export default function TodoList() {
+  const { logoutUser } = bindActionCreators(actionLogin, useDispatch());
   return (
     <header>
       <h1>Task List 2022</h1>
@@ -12,6 +17,9 @@ export default function TodoList() {
       <br />
       <br />
       <TodoBody />
+      <br/>
+      <br/>
+      <Button onClick = {()=>logoutUser()}>Log out</Button>
     </header>
   );
 }
